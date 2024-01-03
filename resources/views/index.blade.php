a1ebfe1ad72923b4a3ebb8a66e7c673e22cac218
Mahadi Hasan Shanto <mahadishanto32@gmail.com>
2024-01-04 00:48:56 +0600

    freelancer

---
+++ resources/views/index.blade.php
@@ -0,0 +1,12 @@
+@include('includes.header')
+@include('includes.navbar')
+@include('includes.masthead')
+@include('includes.portfolio')
+@include('includes.about')
+@include('includes.contact')
+@include('includes.footer-top')
+@include('includes.copyright')
+@include('includes.modal')
+@include('includes.footer')
+      
+        
